3fb5a387a62ec2576f4ccbd61988b6a1310eca1d
Mat Broughton <mat@broughty.com>
2014-08-25 18:31:52 +0100

    angular update stuff

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -7,20 +7,40 @@ bttsApp2.controller("MainController", ["$scope", "$firebase",
 
         $scope.alerts = [];
 
+        $scope.selection = {};
+
         $scope.closeAlert = function(index) {
             $scope.alerts.splice(index, 1);
         };
 
 
         $scope.submitChoices = function () {
-            $scope.emailqueue = angular.copy($scope.selectionqueue);
-            //$scope.selectionqueue = {};
+            $scope.emailqueue = angular.copy($scope.selection);
             $scope.alerts.push({ type: 'success', msg: 'Well done! You successfully submitted your choices.'});
+
+            var ref = new Firebase("https://fourfold.firebaseio.com/week/" + $scope.currentweek.week + "/selections");
+            var sync = $firebase(ref);
+            $scope.selections = sync.$asArray();
+
+            $scope.selection.week = $scope.currentweek.week;
+            var selectionObj = angular.copy($scope.selection);
+
+            if(getByValue($scope.selections, selectionObj.name) === null){
+                $scope.selections.$save(selectionObj);
+            }else{
+                $scope.selections.$add(selectionObj);
+            }
+
+
+
+            $scope.selection= {};
+
         }
 
         this.init = function () {
 
             this.toggle = false;
+
 
             var ref = new Firebase("https://fourfold.firebaseio.com/currentweek");
             // create an AngularFire reference to the data
@@ -30,7 +50,9 @@ bttsApp2.controller("MainController", ["$scope", "$firebase",
             var syncObject = sync.$asObject();
             // synchronize the object with a three-way data binding
             // click on `index.html` above to see it used in the DOM!
-            syncObject.$bindTo(this, "currentweek");
+            syncObject.$bindTo($scope, "currentweek");
+
+
 
 
             var usersRef = new Firebase("https://fourfold.firebaseio.com/users");
@@ -43,17 +65,6 @@ bttsApp2.controller("MainController", ["$scope", "$firebase",
             // click on `index.html` above to see it used in the DOM!
             syncUsersObject.$bindTo(this, "users");
 
-
-            // this is not cleared down by zapper
-            var selQueueRef = new Firebase("https://fourfold.firebaseio.com/selectionqueue");
-            // create an AngularFire reference to the data
-            var syncSelQueue = $firebase(selQueueRef);
-
-            // download the data into a local object
-            var syncSelectionQueueObject = syncSelQueue.$asObject();
-            // synchronize the object with a three-way data binding
-            // click on `index.html` above to see it used in the DOM!
-            syncSelectionQueueObject.$bindTo($scope, "selectionqueue");
 
 
             // this is cleared down by zapper
@@ -69,6 +80,15 @@ bttsApp2.controller("MainController", ["$scope", "$firebase",
 
 
         }
+
+        function getByValue(arr, value) {
+
+            for (var i=0, iLen=arr.length; i<iLen; i++) {
+
+                if (arr[i].name === value) return arr[i];
+            }
+        }
+
 
     }
 ]);
